88016b8e00103b355b4883909955e8f31ad43133
T.Kav <tomas@plienas.lt>
2022-06-10 00:48:56 +0300

    sutvarkytas user delete ir pridetas club member ID per GET su papildomu key  su countDocuments

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
 // is Egidijaus kodo pavyzdyus del env duomenu
 const {
   PORT, DBC, URI, CE
-} =process.env
+} = process.env
 
 
 const client = new MongoClient(URI, {
@@ -30,13 +30,12 @@ const client = new MongoClient(URI, {
 });
 
 app.listen(PORT, () => {
-  console.log('Serveris paleistas. Laukia užklausų');
+  console.log(`Serveris paleistas. Laukia užklausų ant ${PORT} porto`);
 });
 
 app.get('/users', (req, res) => {
   client.connect(async () => {
     const collection = client.db(DBC).collection(CE);
-
     try {
       const result = await collection.find({}).toArray();
       res.json(result);
@@ -55,16 +54,20 @@ app.get('/users', (req, res) => {
 // DB_URI - tokiu budu neliktu nei user namo nei slaptazodio kode
 //  tada process.env.URI / env.DB_name ir t.t.
 
-app.post('/user', (req, res) => {
+app.post('/users', (req, res) => {
   client.connect(async () => {
     const collection = client.db('usersdb').collection('users');
+    const userCount = await collection.countDocuments() + 1
+    // console.log(userCount) //suveikia, gaunu skaiciu kuris atitiks naujo nario eiles numeri
+
     try {
+
       const result = await collection.insertOne({
-        vardas: req.body.vardas,
-        pavarde: req.body.pavarde,
-        metai: req.body.metai,
-        el_pastas: req.body.el_pastas
+        name: req.body.name,
+        age: req.body.age,
+        number: userCount
       });
+      console.log(result)
       res.json(result);
       client.close();
 
@@ -76,12 +79,12 @@ app.post('/user', (req, res) => {
 });
 
 
-app.delete('/user', (req, res) => {
-  console.log(ObjectId(req.body.id))
+app.delete('/users/:id', (req, res) => {
+  const id = req.params.id
   client.connect(async () => {
     const collection = client.db('usersdb').collection('users');
     const result = await collection.deleteOne({
-      _id: ObjectId(req.body.id)
+      _id: ObjectId(id)
     })
     res.json(result);
     client.close();
@@ -89,132 +92,117 @@ app.delete('/user', (req, res) => {
 });
 
 
-app.patch("/user", (req, res) => {
-  client.connect(async function (err, client) {
-    if (err) {
-      res.send("Something went wrong!!");
-      client.close();
-    } else {
-      const database = client.db('usersdb')
-      const collection = database.collection('users')
+// app.patch("/user", (req, res) => {
+//   client.connect(async function (err, client) {
+//     if (err) {
+//       res.send("Something went wrong!!");
+//       client.close();
+//     } else {
+//       const database = client.db('usersdb')
+//       const collection = database.collection('users')
 
-      const { _id, vardas } = req.body;
-      // const _id = req.params.body._id;
-      // const name = req.params.body.name;
-      //82 ir 83 eilutes atitinka 81 eilute
-      const filter = { _id: ObjectId(_id) };
-      const newValues = { $set: { vardas: vardas } }; // gali buti tik vardas
-      // 86 eilutes syntax reikia atsiminti del mongodb PATCH metodo
-      try {
-        const result = await collection.updateOne(filter, newValues);
-        res.send(result);
-        client.close();
-      } catch (err) {
-        res.send("Something went wrong!!");
-        client.close();
-      }
-    }
-  });
-});
+//       const { _id, vardas } = req.body;
+//       // const _id = req.params.body._id;
+//       // const name = req.params.body.name;
+//       //82 ir 83 eilutes atitinka 81 eilute
+//       const filter = { _id: ObjectId(_id) };
+//       const newValues = { $set: { vardas: vardas } }; // gali buti tik vardas
+//       // 86 eilutes syntax reikia atsiminti del mongodb PATCH metodo
+//       try {
+//         const result = await collection.updateOne(filter, newValues);
+//         res.send(result);
+//         client.close();
+//       } catch (err) {
+//         res.send("Something went wrong!!");
+//         client.close();
+//       }
+//     }
+//   });
+// });
 
 // put updatins visa objekta
-app.put("/user", (req, res) => {
-  client.connect(async function (err, client) {
-    if (err) {
-      res.send("Something went wrong!!");
-      client.close();
-    } else {
-      const database = client.db('usersdb')
-      const collection = database.collection('users')
+// app.put("/user", (req, res) => {
+//   client.connect(async function (err, client) {
+//     if (err) {
+//       res.send("Something went wrong!!");
+//       client.close();
+//     } else {
+//       const database = client.db('usersdb')
+//       const collection = database.collection('users')
 
-      const { _id, vardas, pavarde, metai, el_pastas } = req.body;
-      // const _id = req.params.body._id;
-      // const name = req.params.body.name;
-      //82 ir 83 eilutes atitinka 81 eilute
-      const filter = { _id: ObjectId(_id) };
-      const newValues = {
-        vardas: vardas,
-        pavarde: pavarde,
-        metai: metai,
-        el_pastas: el_pastas
-      };
-      try {
-        const result = await collection.replaceOne(filter, newValues);
-        res.send(result);
-        client.close();
-      } catch (err) {
-        res.send("something went wrong")
-        client.close()
-      }
+//       const { _id, vardas, pavarde, metai, el_pastas } = req.body;
+//       // const _id = req.params.body._id;
+//       // const name = req.params.body.name;
+//       //82 ir 83 eilutes atitinka 81 eilute
+//       const filter = { _id: ObjectId(_id) };
+//       const newValues = {
+//         vardas: vardas,
+//         pavarde: pavarde,
+//         metai: metai,
+//         el_pastas: el_pastas
+//       };
+//       try {
+//         const result = await collection.replaceOne(filter, newValues);
+//         res.send(result);
+//         client.close();
+//       } catch (err) {
+//         res.send("something went wrong")
+//         client.close()
+//       }
 
-    }
-  });
-});
+//     }
+//   });
+// });
 
-app.get("/count/:from/:to", (req, res) => {
-  client.connect(async function (err, client) {
-    if (err) {
-      res.send("something went wrong")
-      client.close()
-    } else {
-      const from = Number(req.params.from)
-      const to = Number(req.params.to)
-      const database = client.db('usersdb');
-      const collection = database.collection('users');
-      const result = await collection.countDocuments({ age: { $gt: from, $lt: to } })
-      res.send(`result: ${result}`)
-    }
-  });
-});
+// app.get("/count/:from/:to", (req, res) => {
+//   client.connect(async function (err, client) {
+//     if (err) {
+//       res.send("something went wrong")
+//       client.close()
+//     } else {
+//       const from = Number(req.params.from)
+//       const to = Number(req.params.to)
+//       const database = client.db('usersdb');
+//       const collection = database.collection('users');
+//       const result = await collection.countDocuments({ age: { $gt: from, $lt: to } })
+//       res.send(`result: ${result}`)
+//     }
+//   });
+// });
 
-app.get("/sum/:user", (req, res) => {
-  client.connect(async function (err, client) {
-    if (err) {
-      res.send("something went wrong")
-      client.close()
-    } else {
-      const database = client.db('usersdb');
-      const collection = database.collection('users');
-      const result = await collection
-        .aggregate([
-          { $match: { name: req.params.user } },
-          { $group: { _id: "$name", amziausVidurkis: { $avg: "$age" } } }])
-        .toArray()
-      res.send(result)
-    }
-  });
-});
+// app.get("/sum/:user", (req, res) => {
+//   client.connect(async function (err, client) {
+//     if (err) {
+//       res.send("something went wrong")
+//       client.close()
+//     } else {
+//       const database = client.db('usersdb');
+//       const collection = database.collection('users');
+//       const result = await collection
+//         .aggregate([
+//           { $match: { name: req.params.user } },
+//           { $group: { _id: "$name", amziausVidurkis: { $avg: "$age" } } }])
+//         .toArray()
+//       res.send(result)
+//     }
+//   });
+// });
 
-app.get("/count/:from/:to", (req, res) => {
-  client.connect(async function (err, client) {
-    if (err) {
-      res.send("something went wrong")
-      client.close()
-    } else {
-      const from = Number(req.params.from)
-      const to = Number(req.params.to)
-      const database = client.db('usersdb');
-      const collection = database.collection('users');
-      const result = await collection.countDocuments({ age: { $gt: from, $lt: to } })
-      res.send(`result: ${result}`)
-    }
-  });
-});
-
-app.get("/users", (req, res) => {
-  client.connect(async function (err, client) {
-    if (err) {
-      res.send("something went wrong")
-      client.close()
-    } else {
-      const database = client.db('usersdb');
-      const collection = database.collection('users');
-      const result = await collection
-        .toArray()
-      res.send(result)
-    }
-  });
-});
+// app.get("/count/:from/:to", (req, res) => {
+//   client.connect(async function (err, client) {
+//     if (err) {
+//       res.send("something went wrong")
+//       client.close()
+//     } else {
+//       const from = Number(req.params.from)
+//       const to = Number(req.params.to)
+//       const database = client.db('usersdb');
+//       const collection = database.collection('users');
+//       const result = await collection.countDocuments({ age: { $gt: from, $lt: to } })
+//       res.send(`result: ${result}`)
+//     }
+//   });
+// });
 
 app.get("/count", (req, res) => {
   client.connect(async function (err, client) {
@@ -222,11 +210,11 @@ app.get("/count", (req, res) => {
       res.send("something went wrong")
       client.close()
     } else {
-      
+
       const database = client.db('usersdb');
       const collection = database.collection('users');
-      const result = await collection.countDocuments({ age: { $gt: 50 } })
-      res.send({result})
+      const result = await collection.countDocuments(/*{ age: { $gt: 50 } }*/)
+      res.send({ result })
     }
   });
 });
